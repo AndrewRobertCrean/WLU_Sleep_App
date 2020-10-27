f0c1b0d92557d896d5ff7c77537f214563a7b254
chungb22 <ychungb22@mail.wlu.edu>
2020-10-27 19:51:45 -0400

    10/27/2020-19:50 hour
    1. attempted to put "eofca" boxplot on the ui.r but failed
     1.a. attempted to put relevant codes on server.r but I assume it failed as well
    2. outlined the ui.r which was somewhat successful

--- server.R
+++ server.R
@@ -1,5 +1,48 @@
 # Reading necessary packages
 library(shiny)
 library(shinydashboard)
+library(ggplot2)
+library(tidyverse)
+library(dplyr)
+library(lubridate)
 
-function(input, output) { }
+function(input, output) {
+  
+  #reactive boxplot for Extent of Car Accident Boxplot
+  
+  output$eofcaboxplo <- renderPlot({
+      finimpdam %>%
+        filter(
+          IMPAIRMENT_TYPE %in% input$impairment
+        ) %>%
+        ggplot(
+          aes(
+            IMPAIRMENT_TYPE,
+            PVEH_SEV,
+            fill = IMPAIRMENT_TYPE
+          )
+        )+
+        geom_boxplot(
+          alpha = 0.9
+        ) +
+        theme(
+          axis.text.x = element_text(
+            angle = 60, 
+            hjust = 1
+          )
+        ) +
+        labs(
+          title = "Driver Impairment and Extent of Damage",
+          x = 'Impairment Type',
+          y = "Extent of Damage"
+        ) + 
+        scale_fill_brewer(
+          palette = "Blues"
+        ) +
+        ylim(
+          0, 
+          8
+        )
+    })
+  
+}
--- ui.R
+++ ui.R
@@ -1,18 +1,31 @@
 # Reading necessary packages
-library(shinydashboard)
 library(shiny)
+library(shinydashboard)
+library(ggplot2)
+library(tidyverse)
+library(dplyr)
+library(lubridate)
 
+
+#dashboard outline for the app
 dashboardPage(
+  
+  # Dashboard header  
+  
   dashboardHeader(
-    title = 'WLU Sleep App'
+    title = 'WLU Sleep App',
+    titleWidth = 300
   ),
+  
+  # Dashboard sidebar menu
+
   dashboardSidebar(
     sidebarMenu(
       menuItem(
-        "Introduction", tabName = "introduction", icon = icon("dashboard")
+        "Home", tabName = "home", icon = icon("home")
         ),
       menuItem(
-        "500 Cities Sleep Health Data", tabName = "cities", icon = icon("map")
+        "500 Cities Sleep Health Data", tabName = "cities", icon = icon("map-marked")
         ),
       menuItem(
         "Railroad Dispatchers' Sleep Health", tabName = "railroad", icon = icon("question-circle")
@@ -21,9 +34,79 @@ dashboardPage(
         "Fatiguness on Extent of Car Accident", tabName = "eofca", icon = icon("car-crash")
         ),
       menuItem(
-        "About", tabName = "about", icon = icon("person-circle")
+        "About", tabName = "about", icon = icon("info-circle")
+      )
+    ),
+    
+    #had to fit all of our menu name
+    width = 300,
+    
+    collapsed = FALSE
+  ),
+
+  # Outline the individual tabs in our application
+  dashboardBody(
+    tabItems(
+      #"home"
+      tabItem(
+        tabName = "home",
+        h1(
+          strong("Home")
+        )
+      ),
+      
+      #"cities"
+      tabItem(
+        tabName = "cities",
+        h1(
+          strong("500 Cities Sleep Health Data")
+        )
+      ),
+      #"railroad"
+      tabItem(
+        tabName = "railroad",
+        h1(
+          strong("Railroad Dispatcher's Sleep Health")
+        )
+      ),
+      #"eofca"
+      tabItem(
+        tabName = "eofca",
+        h1(
+          strong("Fatigueness on Extent of Car Accident")
+        )
+      ),
+      #"about"
+      tabItem(
+        tabName = "about",
+        h1(
+          strong("About")
+        ),
+        br(),
+        br(),
+        p(
+          "Paragraph 1 of About",
+          style="font-size:24px"
+          ),
+        br(),
+        p(
+          "Paragraph 2 of About",
+          style="font-size:24px"
+          ),
+        br(),
+        p(
+          "Thanks, ",
+          style="font-size:24px"
+          ),
+        br(),
+        p(
+          "Midha Ahamad, K. Bryan Chung, Andrew Crean",
+          style="font-size:24px"
+          ),
+        br()
       )
     )
-  ),
-  dashboardBody()
+  )
 )
+
+  
