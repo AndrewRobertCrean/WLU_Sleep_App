1045c07f11094cf5b46701589bcaebec273d0b27
chungb22 <ychungb22@mail.wlu.edu>
2020-10-29 13:27:25 -0400

    Organize folders

---
+++ Bryan Chung/Trial and Errors/visualization.R
@@ -0,0 +1,105 @@
+library(ggplot2)
+library(tidyverse)
+
+impdam_comb %>%
+  ggplot(
+    aes(
+      IMPAIRMENT_TYPE,
+       PVEH_SEV,
+      na.rm = TRUE
+        )
+  ) +
+  geom_bar(
+    stat = 'identity'
+  ) + 
+  theme(
+    axis.text.x = element_text(
+      angle = 60, 
+      hjust = 1
+      )
+    )
+
+impdam_comb %>%
+  ggplot(
+    aes(
+      PVEH_SEV,
+      fill = IMPAIRMENT_TYPE
+    )
+  ) +
+  geom_density(
+  alpha = .2
+    )
+
+impdam_comb %>%
+  ggplot(
+    aes(
+      IMPAIRMENT_TYPE,
+      PVEH_SEV
+    )
+  ) +
+  geom_point(
+    
+  ) +
+  theme(
+    axis.text.x = element_text(
+      angle = 60, 
+      hjust = 1
+    )
+  )
+
+impdam_comb %>%
+  ggplot(
+    aes(
+      IMPAIRMENT_TYPE,
+      PVEH_SEV,
+      fill = IMPAIRMENT_TYPE
+    )
+  )+
+  geom_boxplot(
+    alpha = 0.9
+  ) +
+  theme(
+    axis.text.x = element_text(
+      angle = 60, 
+      hjust = 1
+    )
+  ) +
+  labs(
+    title = "Driver Impairment and Extent of Damage",
+    x = 'Impairment Type',
+    y = "Extent of Damage"
+  ) + 
+  scale_fill_brewer(
+    palette = "Blues"
+  )
+
+finimpdam %>%
+  ggplot(
+    aes(
+      IMPAIRMENT_TYPE,
+      PVEH_SEV,
+      fill = IMPAIRMENT_TYPE
+    )
+  )+
+  geom_boxplot(
+    alpha = 0.9
+  ) +
+  theme(
+    axis.text.x = element_text(
+      angle = 60, 
+      hjust = 1
+    )
+  ) +
+  labs(
+    title = "Driver Impairment and Extent of Damage",
+    x = 'Impairment Type',
+    y = "Extent of Damage"
+  ) + 
+  scale_fill_brewer(
+    palette = "Blues"
+  ) +
+  ylim(
+    0, 
+    8
+  )
+
