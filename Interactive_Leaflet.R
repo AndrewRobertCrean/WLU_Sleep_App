59426309bda4c6ab91d388fe844e991e329bb8bf
AndrewRobertCrean <creana22@mail.wlu.edu>
2020-10-30 09:33:18 -0400

    Leaflet Cleaning

--- Interactive_Leaflet.R
+++ Interactive_Leaflet.R
@@ -10,14 +10,14 @@ pal2 <- colorQuantile("RdYlBu", reverse = TRUE, Sleep_and_Geolocation$Data_Value
 
 leaflet(data = Sleep_and_Geolocation_Clean)%>%
   addProviderTiles(providers$Stamen.Toner)%>%
-  addCircles(label = Sleep_and_Geolocation_Clean$CityName, 
+  addCircleMarkers(label = Sleep_and_Geolocation_Clean$CityName, 
              popup = paste("", Sleep_and_Geolocation_Clean$CityName, "<br>",
                            "Risk Level:", Sleep_and_Geolocation_Clean$RiskLevel, "<br>",
                            "Percentage Poor Sleepers:", Sleep_and_Geolocation_Clean$Data_Value, "<br>",
                            "Latitude:", Sleep_and_Geolocation_Clean$lat, "<br>",
                            "Longitude:", Sleep_and_Geolocation_Clean$long),
              color = ~pal2(Data_Value),
-             radius = (Sleep_and_Geolocation_Clean$Data_Value*100)) %>%
+             radius = ~ifelse(Sleep_and_Geolocation_Clean$RiskLevel == "High Risk", 12, 4)) %>%
   addLegend(title = "Percentage Gradient", pal = pal2, values = ~Data_Value, opacity = 1)
 
 
