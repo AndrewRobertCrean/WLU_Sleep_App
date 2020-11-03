a868b532368c09b1031840bc33f4afcd91290bfa
AndrewRobertCrean <creana22@mail.wlu.edu>
2020-11-03 12:53:17 -0500

    sourced leaflet

--- server.R
+++ server.R
@@ -10,6 +10,7 @@ library(leaflet)
 #Pull in requried data frames
 source("Bryan Chung/Driver Impairment and Extent of Damage final.R")
 source("~/Sleep_App/Andrew Crean/500 Cities Cleaning.R")
+source("~/Sleep_App/Interactive_Leaflet.R")
 
 #500 Cities
 
