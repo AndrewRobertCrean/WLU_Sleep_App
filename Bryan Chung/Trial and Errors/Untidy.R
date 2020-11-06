b7740c20acdb536357824af6e940fe8a9143f26d
chungb22 <ychungb22@mail.wlu.edu>
2020-11-06 01:34:57 -0500

    tried out the graph that Dr. Whitworth suggested; somewhat lost

---
+++ Bryan Chung/Trial and Errors/Untidy.R
@@ -0,0 +1,38 @@
+library(tidyverse)
+
+untidyimp <- finimpdam %>%
+  group_by(
+    ST_CASE
+  ) %>%
+  mutate(
+    indi_case = row_number()
+  ) %>%
+  spread(
+    IMPAIRMENT_TYPE,
+    PVEH_SEV
+  )
+  
+  untidyimp %>%
+    ggplot(
+      aes(
+        `Asleep or Fatigued`
+      )
+    ) +
+    geom_density(
+      na.rm = FALSE
+    )
+  
+finimpdam %>%
+  ggplot(
+    aes(
+      PVEH_SEV,
+      fill = IMPAIRMENT_TYPE
+    )
+  ) +
+  geom_density(alpha =.5)
+  
+  mean(untidyimp$DUI, na.rm = TRUE)
+  mean(untidyimp$`Asleep or Fatigued`, na.rm = TRUE)
+
+  plot(untidyimp$DUI, untidyimp$`Asleep or Fatigued`)  
+  
